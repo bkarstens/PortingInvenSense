1bdcc07c8559cab9f5d2e3b5daf3a6cb14ecacc7
Barrett Karstens <barrettkarstens@gmail.com>
2020-01-04 22:35:20 -0800

    Burst Write does not work so writing in a for loop will be used

--- ASF4 Test project/ASF4ProjectNo.6/main.c
+++ ASF4 Test project/ASF4ProjectNo.6/main.c
@@ -20,29 +20,33 @@ void inv_icm426xx_sleep_us(uint32_t us)
  */
 uint64_t inv_icm426xx_get_time_us(void)
 {
-	return TIMER_0.time*(uint64_t)1000;
+	return TIMER_0.time * (uint64_t)1000;
 }
 
-int read_reg_1(struct inv_icm426xx_serif * serif, uint8_t reg, uint8_t * buf, uint32_t len)
-{
-	int address_written = 1;
-	int bytes_read = 0;
-	struct io_descriptor *io;
-	spi_m_sync_get_io_descriptor(serif->context, &io);
-	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
-	for (uint8_t i = 0; i < len; ++i)                      /* for each register                                  */
-	{
-		spi_m_sync_enable(serif->context);                 /* enable SPI communication                           */
-		gpio_set_pin_level(SN_CS,false);                   /* enable chip select (active low)                    */
-		uint8_t addr = (reg + i) | 0x80;                   /* set the read flag                                  */
-		address_written &= io_write(io, &addr, 1);         /* tell the chip that we want to read from a register */
-		bytes_read      += io_read(io, &buf[i], 1);        /* read from the register                             */
-		gpio_set_pin_level(SN_CS,true);                    /* disable chip select (active low)                   */
-		spi_m_sync_disable(serif->context);                /* disable SPI communication                          */
-	}
-	return !(address_written==1 && bytes_read==len);
-}
+// Unnecessary b/c burst read works
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// int read_reg_1(struct inv_icm426xx_serif * serif, uint8_t reg, uint8_t * buf, uint32_t len)
+// {
+// 	int address_written = 1;
+// 	int bytes_read = 0;
+// 	struct io_descriptor *io;
+// 	spi_m_sync_get_io_descriptor(serif->context, &io);
+// 	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
+// 	spi_m_sync_enable(serif->context);                 /* enable SPI communication                           */
+// 	for (uint8_t i = 0; i < len; ++i)                  /* Read from each register                            */
+// 	{
+// 		gpio_set_pin_level(SN_CS, false);              /* enable chip select (active low)                    */
+// 		uint8_t addr = (reg + i) | 0x80;               /* set the read flag                                  */
+// 		address_written &= io_write(io, &addr, 1);     /* tell the chip that we want to read from a register */
+// 		bytes_read += io_read(io, &buf[i], 1);         /* read from the register                             */
+// 		gpio_set_pin_level(SN_CS, true);               /* disable chip select (active low)                   */
+// 	}
+// 	spi_m_sync_disable(serif->context);                /* disable SPI communication                          */
+// 	return !(address_written==1 && bytes_read==len);
+// }
 
+// Burst write does not work. Example project just writes one byte at a time in a loop; I guess it's because it cant burst write?
+// see inv_io_hal_write_reg in invensense\sources\examples\system-interface\system-interface.c
 int write_reg_1(struct inv_icm426xx_serif * serif, uint8_t reg, const uint8_t * buf, uint32_t len)
 {
 	int address_written = 1;
@@ -51,23 +55,24 @@ int write_reg_1(struct inv_icm426xx_serif * serif, uint8_t reg, const uint8_t * 
 	spi_m_sync_get_io_descriptor(serif->context, &io);
 	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
 
+	spi_m_sync_enable(serif->context);               /* enable SPI communication                           */
+													 /* Write each byte separately                         */
 	for (uint8_t i = 0; i < len; ++i)
 	{
-		spi_m_sync_enable(serif->context);             /* enable SPI communication                           */
-		gpio_set_pin_level(SN_CS,false);               /* enable chip select (active low)                    */
-		uint8_t addr = (reg + i) & 0x7F;               /* .................                                  */
-		address_written &= io_write(io, &addr, 1);     /* tell the chip that we want to write to a register  */
-		bytes_written   += io_write(io, &buf[i], 1);   /* write to the register                              */
-		gpio_set_pin_level(SN_CS,true);                /* disable chip select (active low)                   */
-		spi_m_sync_disable(serif->context);            /* disable SPI communication                          */
+		gpio_set_pin_level(SN_CS,false);             /* enable chip select (active low)                    */
+		uint8_t addr = (reg + i) & 0x7F;			 /* make sure the read flag is not set                 */
+		address_written &= io_write(io, &addr, 1);   /* tell the chip that we want to write to a register  */
+		bytes_written   += io_write(io, &buf[i], 1); /* write to the register                              */
+		gpio_set_pin_level(SN_CS, true);             /* disable chip select (active low)                   */
 	}
+	spi_m_sync_disable(serif->context);              /* disable SPI communication                          */
 	return !(address_written==1 && bytes_written==len);
 }
 
 int read_reg_2(struct inv_icm426xx_serif * serif, uint8_t reg, uint8_t * buf, uint32_t len)
 {
-	int address_written = 1;
-	int bytes_read = 0;
+	int address_written;
+	int bytes_read;
 	struct io_descriptor *io;
 	spi_m_sync_get_io_descriptor(serif->context, &io);
 	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
@@ -82,22 +87,25 @@ int read_reg_2(struct inv_icm426xx_serif * serif, uint8_t reg, uint8_t * buf, ui
 	return !(address_written==1 && bytes_read==len);
 }
 
-int write_reg_2(struct inv_icm426xx_serif * serif, uint8_t reg, const uint8_t * buf, uint32_t len)
-{
-	int address_written = 1;
-	int bytes_written = 0;
-	struct io_descriptor *io;
-	spi_m_sync_get_io_descriptor(serif->context, &io);
-	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
-
-	spi_m_sync_enable(serif->context);                /* enable SPI communication                           */
-	gpio_set_pin_level(SN_CS,false);                  /* enable chip select (active low)                    */
-	address_written = io_write(io, &reg, 1);          /* tell the chip that we want to write to a register  */
-	bytes_written   = io_write(io,  buf, len);        /* write to the register                              */
-	gpio_set_pin_level(SN_CS,true);                   /* disable chip select (active low)                   */
-	spi_m_sync_disable(serif->context);               /* disable SPI communication                          */
-	return !(address_written==1 && bytes_written==len);
-}
+// This does not work. Example project just writes one byte at a time in a loop; I guess it's because it cant burst write?
+// see inv_io_hal_write_reg in invensense\sources\examples\system-interface\system-interface.c
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// int write_reg_2(struct inv_icm426xx_serif * serif, uint8_t reg, const uint8_t * buf, uint32_t len)
+// {
+// 	int address_written;
+// 	int bytes_written;
+// 	struct io_descriptor *io;
+// 	spi_m_sync_get_io_descriptor(serif->context, &io);
+// 	ASSERT(serif->serif_type == ICM426XX_UI_SPI4);
+// 
+// 	spi_m_sync_enable(serif->context);                /* enable SPI communication                           */
+// 	gpio_set_pin_level(SN_CS,false);                  /* enable chip select (active low)                    */
+// 	address_written = io_write(io, &reg, 1);          /* tell the chip that we want to write to a register  */
+// 	bytes_written   = io_write(io,  buf, len);        /* write to the register                              */
+// 	gpio_set_pin_level(SN_CS,true);                   /* disable chip select (active low)                   */
+// 	spi_m_sync_disable(serif->context);               /* disable SPI communication                          */
+// 	return !(address_written==1 && bytes_written==len);
+// }
 
 
 uint16_t acceldata[3];
@@ -144,7 +152,7 @@ static void setupInvensense(struct inv_icm426xx * p_sensor)
 	struct inv_icm426xx_serif icm_serif;
 	icm_serif.context   = &SPI_5; /*using context as the spi_m_sync_descriptor to use */
 	icm_serif.read_reg  = read_reg_2;
-	icm_serif.write_reg = write_reg_2;
+	icm_serif.write_reg = write_reg_1;
 	icm_serif.max_read  = 1024*32;  /* maximum number of bytes allowed per serial read  (from InvenSense examples) */
 	icm_serif.max_write = 1024*32;  /* maximum number of bytes allowed per serial write (from InvenSense examples) */
 	icm_serif.serif_type = ICM426XX_UI_SPI4;
@@ -189,44 +197,41 @@ void testReadWrite(read_reg_t read_fn, write_reg_t write_fn)
 	/************************************************************************/
 	/* Multiple Read/Write                                                  */
 	/************************************************************************/
-	status |= read_fn(&icm_serif, MPUREG_INT_SOURCE0, buf, 2); /* burst read int_source0/int_source1 */
-	status |= read_fn(&icm_serif, MPUREG_INT_SOURCE1, &buf[2], 1);
-	//beesert(buf[1]=buf[2]);
-	buf[0] &= 0x80;
-	buf[1] |= 0x45;
-	status |= write_fn(&icm_serif, MPUREG_INT_SOURCE0, buf, 2); /* burst write int_source0/int_source1 */
-	buf[0]=0xBB;
-	buf[1]=0xCC;
-	buf[2]=0xDD;
-	status |= read_fn(&icm_serif, MPUREG_INT_SOURCE0, buf, 2); /* burst read int_source0/int_source1 */
-	status |= read_fn(&icm_serif, MPUREG_INT_SOURCE1, &buf[2], 1);
-	//beesert(buf[0]&(~0x80)==0x55); /* was set ignoring reserved bits */
-	//beesert(buf[1]&(~0x30)==0x85); /* was set ignoring reserved bits */
-	//beesert(buf[1]==buf[2]);
-	
-	/************************************************************************/
-	/* TODO: Bank switching                                                 */
-	/************************************************************************/
-	
+	buf[0]=4;
+	status |= write_fn(&icm_serif, MPUREG_REG_BANK_SEL,buf,1);      // switch to bank 4
+	status |= read_fn(&icm_serif, MPUREG_OFFSET_USER_0_B4, buf, 2); /* burst read OFFSET_USER0 and OFFSET_USER1 registers */
+	status |= read_fn(&icm_serif, MPUREG_OFFSET_USER_1_B4, &buf[2], 1);
+	buf[0] = 0xAB;
+	buf[1] = 0xCD;
+	status |= write_fn(&icm_serif, MPUREG_OFFSET_USER_0_B4, buf, 2); /* burst write OFFSET_USER0 and OFFSET_USER1 registers */
+	buf[0]=0xFF;
+	buf[1]=0xFF;
+	buf[2]=0xFF;
+	status |= read_fn(&icm_serif, MPUREG_OFFSET_USER_0_B4, buf, 2); /* burst read OFFSET_USER0 and OFFSET_USER1 registers */
+	status |= read_fn(&icm_serif, MPUREG_OFFSET_USER_1_B4, &buf[2], 1);
+	beesert(buf[0]==0xAB);
+	beesert(buf[1]==0xCD);
+	beesert(buf[1]==buf[2]);
 	
 	beesert(status==0);
 }
 uint64_t currentTime;
 int main(void)
 {
-	// I have to malloc b/c for some reason the compiler keeps putting this on top of SPI_5 which corrupts it... 
+	// I have to malloc b/c for some reason the compiler keeps putting this on top of SPI_5 which corrupts it...
+	// maybe 2,176 Byte struct is too big for the stack?
 	//*
 	struct inv_icm426xx *sensor = malloc(sizeof(struct inv_icm426xx));
 	/*/
 	struct inv_icm426xx sensor_loc;
 	struct inv_icm426xx *sensor = &sensor_loc;
 	//*/
+	ASSERT(!((char*)sensor < (char*)&SPI_5 && (char*)&SPI_5 < ((char*)sensor + sizeof *sensor)));
 	
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 	
-	testReadWrite(read_reg_2,write_reg_2);
-	//testReadWrite(read_reg_1,write_reg_1);
+	//testReadWrite(read_reg_2,write_reg_1);
 	
 	timer_start(&TIMER_0);
 	
